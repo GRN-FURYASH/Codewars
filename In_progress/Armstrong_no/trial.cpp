ad9ba92866a216053b7f9f7229a12b94e33ac898
GRN-FURYASH <agarwalbabita519@gmail.com>
2023-07-22 21:22:38 +0530

    to be worked on

---
+++ In_progress/Armstrong_no/trial.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+
+class Solution {
+    bool is_arm_num(int num) {
+
+        int current_digit;
+        int sum = 0;
+
+        if (num == 0) {
+
+            if (sum == num) {
+                return true;
+            }
+
+            else {
+                return false;
+            }
+
+        }
+
+
+
+    }
+};
